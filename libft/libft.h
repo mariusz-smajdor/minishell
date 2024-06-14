d890b329a3fda21ad28a92ea2e0ca2227ca736ea
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-06-14 10:09:35 +0000

    add ft_isspace function to libft

--- libft/libft.h
+++ libft/libft.h
@@ -34,6 +34,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_isspace(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
