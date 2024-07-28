298387fa5d53c86785558886c2c2c94d1bc1f53b
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-07-28 18:16:51 +0000

    exit_program util function

---
+++ utils/exit_program.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_program.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 22:15:00 by msmajdor          #+#    #+#             */
+/*   Updated: 2024/06/03 22:15:01 by msmajdor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void	exit_program(const char *message, const int status)
+{
+	printf("%s\n", message);
+	exit(status);
+}
