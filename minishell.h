e982722e78c93ebed1c601b49d8500b749edc88c
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-07-28 18:03:54 +0000

    datamodel for commands storage

--- minishell.h
+++ minishell.h
@@ -1,8 +1,24 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef enum e_tokens
+{
+	PIPE,
+	GREAT,
+	GREAT_GREAT,
+	LESS,
+	LESS_LESS,
+}	t_tokens;
+
+typedef struct s_command
+{
+	char		**cmds;
+	t_tokens	token;
+	t_command	*next;
+}	t_command;
 
 #endif
