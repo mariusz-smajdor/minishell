af53c93a781c64d3b05b07083853a1a9f74f7a15
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-08-03 02:12:09 +0000

    typedefs for tokenization

--- minishell.h
+++ minishell.h
@@ -7,6 +7,24 @@
 # include <readline/history.h>
 # include "libft/libft.h"
 
+typedef enum e_ops
+{
+	NONE,
+	PIPE,
+	LT,
+	GT,
+	LTLT,
+	GTGT,
+	END,
+}	t_ops;
+
+typedef struct s_token
+{
+	char	*value;
+	t_ops	operator;
+	t_token	*next;
+}	t_token;
+
 // UTILS
 void	exit_program(const char *message, const int status);
 void	*safe_malloc(size_t size);
