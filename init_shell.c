7de7e44d623783fd854a12e7d48f5ec23273028b
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-08-18 15:34:11 +0200

    change mallocs to safe_mallocs

--- init_shell.c
+++ init_shell.c
@@ -6,7 +6,7 @@
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:34:13 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/08/17 19:34:34 by msmajdor         ###   ########.fr       */
+/*   Updated: 2024/08/18 15:33:34 by msmajdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ static t_envp	*init_envp(char **envp)
 	size_t	i;
 
 	i = 0;
-	env = malloc(sizeof(t_envp));
+	env = safe_malloc(sizeof(t_envp));
 	tmp = env;
 	while (envp[i])
 	{
 		tmp->key = ft_strndup(envp[i], ft_strchr(envp[i], '=') - envp[i]);
 		tmp->value = ft_strdup(ft_strchr(envp[i], '=') + 1);
-		tmp->next = malloc(sizeof(t_envp));
+		tmp->next = safe_malloc(sizeof(t_envp));
 		tmp = tmp->next;
 		i++;
 	}
@@ -35,7 +35,7 @@ static t_envp	*init_envp(char **envp)
 
 void	init_shell(t_shell *shell, char **envp)
 {
-	shell->cmd = malloc(sizeof(t_cmd));
+	shell->cmd = safe_malloc(sizeof(t_cmd));
 	shell->envp = init_envp(envp);
 	shell->exit = false;
 }
