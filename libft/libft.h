2ea912be01127e2ce96b1b75d1873b4079edd48d
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-08-29 11:32:24 +0000

    add ft_strchrn function

--- libft/libft.h
+++ libft/libft.h
@@ -40,6 +40,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_strchrn(char *s, int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
